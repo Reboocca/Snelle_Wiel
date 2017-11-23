d52d3855a98e465eb7b62ecd1af56000d95b485e
Unknown <rebeccabroens@gmail.com>
2017-11-23 15:31:57 +0100

    SNWL planning + vrachtwagen - aanpassingen
    kleine aanpassingen aan planning app
    opzet gemaakt voor vrachtwagen app
    heel inlogscherm vrachtwagen app af

---
+++ Snelle_Wiel/Ontwikkelen/SNWL_VrachtwagenApp/SNWL_VrachtwagenApp/SNWL_VrachtwagenApp/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SNWL_VrachtwagenApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            imgLogo.Source = ImageSource.FromResource("SNWL_VrachtwagenApp.img.logo.png");
+        }
+    }
+}
